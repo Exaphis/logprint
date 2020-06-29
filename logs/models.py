75216056a4b89baee7bb723e4a52ac9a2adfc2c7
Exaphis <kevin@kevinniuwu.com>
2020-06-28 18:10:47 -0700

    Add logs models

--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class LogCollection(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    create_time = models.DateTimeField()
+
+
+class Log(models.Model):
+    collection = models.ForeignKey(LogCollection, on_delete=models.CASCADE)
+    log_text = models.TextField()
+    ip_addr = models.GenericIPAddressField()
+    received_time = models.DateTimeField()
