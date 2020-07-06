e2db655ba66541a682aa652223b4d2f6d7083fa0
Exaphis <kevin@kevinniuwu.com>
2020-07-05 23:17:37 -0700

    Use class-based view for index

--- logs/urls.py
+++ logs/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'logs'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('<uuid:collection_unique_id>/', views.view_collection, name='view-collection')
 ]
--- logs/views.py
+++ logs/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.views import generic
 
-from .models import LogCollection
+from .models import LogCollection, Log
 
 
-# Create your views here.
-def index(request):
-    collections_list = LogCollection.objects.order_by('create_time')
-    context = {'collections_list': collections_list}
-    return render(request, 'logs/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'logs/index.html'
+    context_object_name = 'collections_list'
+
+    def get_queryset(self):
+        """Return all LogCollections, sorted by creation date."""
+        return LogCollection.objects.order_by('create_time')
 
 
 def view_collection(request, collection_unique_id):
     collection = get_object_or_404(LogCollection, unique_id=collection_unique_id)
-    return HttpResponse(f'Collection id: {collection_unique_id}')
+    logs = Log.objects.filter(collection=collection)
+    context = {'collection': collection, 'logs': logs}
+    return render(request, 'logs/view_collection.html', context)
